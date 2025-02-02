baabdbe47457436b70339180aa30eed4ca662456
burce <burcev-egor@mail.ru>
2025-02-02 08:24:43 +0500

    protoc

--- protoc.sh
+++ protoc.sh
@@ -9,9 +9,10 @@ git config user.name "$USER_NAME"
 git config user.email "$EMAIL"
 git fetch --all && git checkout main
 
-sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
-go get google.golang.org/protobuf/cmd/protoc-gen-go@latest
-go get google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+sudo apt-get install -y protobuf-compiler
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+sudo apt-get upgrade -y
 protoc --go_out=./golang --go_opt=paths=source_relative \
   --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
  ./${SERVICE_NAME}/*.proto
