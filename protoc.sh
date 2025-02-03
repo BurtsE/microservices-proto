79d5b6a6295c88c34b35094acf4e26520ef739ff
burce <burcev-egor@mail.ru>
2025-02-03 16:17:13 +0500

    deploy

--- protoc.sh
+++ protoc.sh
@@ -12,7 +12,6 @@ git fetch --all && git checkout main
 sudo apt-get install -y protobuf-compiler
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-sudo apt-get upgrade -y
 protoc --go_out=./golang --go_opt=paths=source_relative \
   --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
  ./${SERVICE_NAME}/*.proto
